68f4e9f16125789c576eeb5d849b9e368575159e
yenugantikarthik <yenugantikarthik8@gmail.com>
2021-08-10 23:51:26 -0700

    Python string file

---
+++ python strings.py
@@ -0,0 +1,18 @@
+parrot = 'Norwegian Blue'
+#print(parrot[0])
+
+print(parrot[3]+'\n')
+print(parrot[4]+'\n')
+print()
+print(parrot[3]+'\n')
+print(parrot[6]+'\n')
+print(parrot[8]+'\n')
+#--------------------------------------
+list = parrot.split()
+st = str(list[0])
+ne = [ne for ne in st]
+print(ne)
+b = ''
+for i in ne :
+     b = b+i
+print(b)       
