0002f2760941bb97a8ba950fad5b31fe49062f86
yenugantikarthik <yenugantikarthik8@gmail.com>
2021-08-11 07:13:47 -0700

    python string file 1

--- python string1.py
+++ python string1.py
@@ -1,1 +1,10 @@
-#hello 
+parrot = 'Norwegian Blue'
+
+#print(parrot[-1])
+
+print(parrot[3-14])
+print(parrot[4-14])
+print(parrot[9-14])
+print(parrot[3-14])
+print(parrot[6-14])
+print(parrot[8-14])
